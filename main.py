522c67f3b63c7c74cca4eceda94064e2131d3335
rehansanish <94996859+rehansanish@users.noreply.github.com>
2021-11-29 18:11:54 -0500

    calculator

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
 print("Calculator")
+
+print("Type in the operation you want to perform")
+print("Options are: multiply, divide, add or subtract")
+
+operation = input()
+
+if operation == "add":
+  print("type in a number")
+  firstnum = input()
+  print("type in second number")
+  secondnum = input()
+  print(firstnum + secondnum)
+
+if operation == "multiply":
+  print("type in a number")
+  firstnum = input()
+  print("type in second number")
+  secondnum = input()
+  print(firstnum * secondnum)
+
+if operation == "subtract":
+  print("type in a number")
+  firstnum = input()
+  print("type in second number")
+  secondnum = input()
+  print(firstnum - secondnum)
+
+if operation == "divide":
+  print("type in a number")
+  firstnum = input()
+  print("type in second number")
+  secondnum = input()
+  print(firstnum / secondnum)
